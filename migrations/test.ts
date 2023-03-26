f8f55f7da9f9ed175df883cbb25ab7a52dfcf254
Issackr <issackr@metisdata.io>
2023-03-26 18:39:41 +0300

    test

---
+++ migrations/test.ts
@@ -0,0 +1,46 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+module.exports = {
+  up: async (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
+    await queryInterface.createTable('MyUsers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+      },
+      bio: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.dropTable('anotherTable');
+  },
+  down: async (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
+    await queryInterface.dropTable('MyUsers');
+    await queryInterface.createTable('anotherTable', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+};
